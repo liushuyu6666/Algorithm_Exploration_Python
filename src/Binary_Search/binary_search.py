78a887b8d48a0fd329d31ffb769a90abc5e92681
shuyu liu <liushuyu6666@gmail.com>
2023-12-21 01:49:54 -0500

    update both_included_both_strict_right make middle = math.ceil((left + right) / 2)

--- src/Binary_Search/binary_search.py
+++ src/Binary_Search/binary_search.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 #  Please first understand
@@ -26,7 +27,7 @@ class BinarySearch:
         left, right = 0, len(sorted_nums) - 1
 
         while left < right:  # end condition: left == right
-            middle = (left + right) // 2 + 1
+            middle = math.ceil((left + right) / 2)
             if sorted_nums[middle] > target:  # right strict
                 right = middle - 1
             else:
